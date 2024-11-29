d260602661eed1c46a3ef4bfd52f17b14ad61f2f
Thomas Nowak <thomas@thomasnowak.net>
2024-11-29 11:14:30 +0100

    Moved CSVReader from "file" to "source"

--- logicsponge/core/source.py
+++ logicsponge/core/source.py
@@ -1,3 +1,5 @@
+import csv
+import logging
 import os
 import tempfile
 import time
@@ -10,6 +12,14 @@ import watchdog.observers
 from watchdog.observers.api import BaseObserver
 
 import logicsponge.core as ls
+
+# logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    encoding="utf-8",
+    format="%(message)s",
+    level=logging.INFO,
+)
 
 
 class FileWatchHandler(watchdog.events.FileSystemEventHandler):
@@ -60,6 +70,37 @@ class FileWatchSource(ls.SourceTerm):
 
     def run(self):
         time.sleep(60)  # TODO: better way?
+
+
+class CSVStreamer(ls.SourceTerm):
+    def __init__(self, *args, file_path: str, delay: float = 0, poll_delay: float = 1, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_path = file_path
+        self.delay = delay
+        self.poll_delay = poll_delay
+        self.position = 0
+
+    def run(self):
+        while True:
+            try:
+                with open(self.file_path) as csvfile:
+                    # Move the pointer to the last read position
+                    csvfile.seek(self.position)
+
+                    # Read new lines if available
+                    reader = csv.DictReader(csvfile)
+                    for row in reader:
+                        time.sleep(self.delay)
+                        out = ls.DataItem(row)
+                        self.output(out)
+
+                    self.position = csvfile.tell()
+
+                time.sleep(self.poll_delay)
+            except Exception as e:
+                msg = f"Error while streaming CSV: {e}"
+                logger.exception(msg)
+                break
 
 
 class GoogleDriveSource(ls.SourceTerm):
